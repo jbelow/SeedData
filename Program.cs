a22b2f5d536484cf7864e809a1daf2bd560c1c54
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-01-19 10:12:04 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeedData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
