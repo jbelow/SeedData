56a499356db684dc2f3198da69beb945c347ceed
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-01-19 11:27:25 -0600

    everything is working but the DateTime

--- SeedDataContext.cs
+++ SeedDataContext.cs
@@ -5,7 +5,7 @@ namespace SeedData
     public class SeedDataContext : DbContext
     {
         public DbSet<Location> Location { get; set; }
-        public DbSet<SeedEvent> SeedEvents { get; set; }
+        public DbSet<SeedEvent> SeedEvent { get; set; }
 
         public void AddLocation(Location location)
         {
@@ -15,7 +15,7 @@ namespace SeedData
 
         public void AddSeedEvent(SeedEvent seedEvent)
         {
-            this.SeedEvent.Add(seedevent);
+            this.SeedEvent.Add(seedEvent);
             this.SaveChanges();
         }
 
