adaef6bae341bea4c87be0e0e2fddee7911128b9
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-01-19 10:24:12 -0600

    first commit

---
+++ Location.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+namespace SeedData
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SeedEvent.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace SeedData
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public bool Flagged { get; set; }
+        // foreign key for location 
+        public int LocationId { get; set; }
+        // navigation property
+        public Location Location { get; set; }
+    }
+}
