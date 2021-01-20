93a551f56d28f5955699eb62714a671bc36c2350
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-01-19 18:53:15 -0600

    //

--- Program.cs
+++ Program.cs
@@ -14,31 +14,31 @@ namespace SeedData
         {
             logger.Info("Program started");
 
-            try
-            {
-                // Create and save a new Location
-                Console.Write("Enter a name for a new Location: ");
-                var name = Console.ReadLine();
+            // try
+            // {
+            //     // Create and save a new Location
+            //     Console.Write("Enter a name for a new Location: ");
+            //     var name = Console.ReadLine();
 
-                var location = new Location { Name = name };
+            //     var location = new Location { Name = name };
 
-                var db = new SeedDataContext();
-                db.AddLocation(location);
-                logger.Info("Location added - {name}", name);
+            //     var db = new SeedDataContext();
+            //     db.AddLocation(location);
+            //     logger.Info("Location added - {name}", name);
 
-                // Display all Locations from the database
-                var query = db.Location.OrderBy(b => b.Name);
+            //     // Display all Locations from the database
+            //     var query = db.Location.OrderBy(b => b.Name);
 
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
-                {
-                    Console.WriteLine(item.Name);
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex.Message);
-            }
+            //     Console.WriteLine("All blogs in the database:");
+            //     foreach (var item in query)
+            //     {
+            //         Console.WriteLine(item.Name);
+            //     }
+            // }
+            // catch (Exception ex)
+            // {
+            //     logger.Error(ex.Message);
+            // }
 
             logger.Info("Program ended");
         }
